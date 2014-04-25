95fcc014997d1776b259b76d58438a382a8ae247
qianfeng <apple@aofel.local>
2014-04-25 19:10:19 +0800

    Initial Commit

---
+++ CityBlocks_4.25/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CityBlocks_4.25
+//
+//  Created by qianfeng on 14-4-25.
+//  Copyright (c) 2014年 aofel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
